2a28b40d3d63b109e165038418a0c94bd7212e5f
Даниил <bronnikovds@gmail.com>
2025-04-05 21:23:41 +0500

    score of mines

---
+++ src/components/Mines.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const props = defineProps<{ count: number }>();
+</script>
+
+<template>
+    <BBadge class="fs-3" variant="danger">
+        💣
+        {{ props.count }}
+    </BBadge>
+</template>
